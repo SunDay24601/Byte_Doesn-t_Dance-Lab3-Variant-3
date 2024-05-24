c3620d3a1476ce5dd2bc4c68f823cbc04bd65483
Hu Jinghao <1206041060@qq.com>
2024-05-24 20:43:26 +0300

    visualization

--- Mealy_test.py
+++ Mealy_test.py
@@ -59,6 +59,24 @@ class TestMealyFSM(unittest.TestCase):
         self.assertEqual(fsm.state_seq, expected_state_sequence)
         self.assertEqual(fsm.output_seq, expected_output_sequence)
 
+    def test_dot_graph_generation(self) -> None:
+        fsm = MealyFSM(initial_state="S0")
+        fsm.add_node("S0", {1: "S1", 0: "S0"}, {1: "Output_A", 0: "Output_B"})
+        fsm.add_node("S1", {1: "S1", 0: "S0"}, {1: "Output_C", 0: "Output_D"})
+
+        expected_dot_graph = """digraph MealyFSM {
+    rankdir=LR;
+    S0 -> S1 [label="1 / Output_A"];
+    S0 -> S0 [label="0 / Output_B"];
+    S1 -> S1 [label="1 / Output_C"];
+    S1 -> S0 [label="0 / Output_D"];
+}"""
+        generated_dot_graph = fsm.generate_dot_graph()
+        self.assertEqual(generated_dot_graph, expected_dot_graph)
+
+        with open("mealy_fsm.dot", "w") as file:
+            file.write(generated_dot_graph)
+
     def test_complex_sequence_detector(self) -> None:  # to detect 11010
         fsm = MealyFSM(initial_state="S")
         fsm.add_node("S", {1: "S1", 0: "S"}, {1: None, 0: None})
@@ -92,20 +110,24 @@ class TestMealyFSM(unittest.TestCase):
 
         self.assertEqual(fsm.output_seq, expected_output_sequence)
 
-    def test_dot_graph_generation(self) -> None:
-        fsm = MealyFSM(initial_state="S0")
-        fsm.add_node("S0", {1: "S1", 0: "S0"}, {1: "Output_A", 0: "Output_B"})
-        fsm.add_node("S1", {1: "S1", 0: "S0"}, {1: "Output_C", 0: "Output_D"})
-
         expected_dot_graph = """digraph MealyFSM {
     rankdir=LR;
-    S0 -> S1 [label="1 / Output_A"];
-    S0 -> S0 [label="0 / Output_B"];
-    S1 -> S1 [label="1 / Output_C"];
-    S1 -> S0 [label="0 / Output_D"];
+    S -> S1 [label="1 / None"];
+    S -> S [label="0 / None"];
+    S1 -> S11 [label="1 / None"];
+    S1 -> S [label="0 / None"];
+    S11 -> S11 [label="1 / None"];
+    S11 -> S110 [label="0 / None"];
+    S110 -> S1101 [label="1 / None"];
+    S110 -> S [label="0 / None"];
+    S1101 -> S11 [label="1 / None"];
+    S1101 -> S [label="0 / Succeed"];
 }"""
         generated_dot_graph = fsm.generate_dot_graph()
         self.assertEqual(generated_dot_graph, expected_dot_graph)
+
+        with open("detector.dot", "w") as file:
+            file.write(generated_dot_graph)
 
     def test_state_table_generation(self) -> None:
         fsm = MealyFSM(initial_state="S0")
